e874cf433a94a2f46c52396205e2438956fdc5b6
Rahul Rajendran <rahulrajendran10098@gmail.com>
2025-12-26 12:02:48 +0530

    feature: added answer list init

---
+++ resources/t.py
@@ -0,0 +1,9 @@
+with open(r"resources\source.txt", 'r') as f, open(r"resources\answer_list.txt", 'w') as out:
+    
+    while line := f.readline():
+        for word in line.split():
+            if len(word) == 5:
+                print(word)
+                out.write(word + '\n')
+            else:
+                print(f"'{word}' is not 5 letters long")
