03d1a624dc61cfdd9db6d3b272d3b779495a2dd0
Rahul Rajendran <rahulrajendran10098@gmail.com>
2025-12-27 21:31:03 +0530

    refactor: move domain logic to domain modules

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-from typing import Union, Any
-
 from fastapi import FastAPI
-from wordle import guess_attempt
-from random import randint
+from wordle.engine import guess_attempt
 from pydantic import BaseModel
+from wordle.get_answer import get_answer
+from wordle.rules import response_builder, validate_guess
 
 
 app = FastAPI()
@@ -14,12 +13,11 @@ class GuessResponse(BaseModel):
     result: list[str]
 
 
-with open("resources/answer_list.txt", "r") as f:
-    answer_list: list[str] = [w.strip() for w in f if w.strip()]
-
-answer = answer_list[randint(0, len(answer_list) - 1)]
+answer = get_answer()
 
 print(answer)
+
+
 @app.get("/")
 def main():
     return {"message": "Welcome to Wordle API! Guess using /guess/<your guess word>"}
@@ -29,23 +27,12 @@ def main():
 def user_guess(guess_word: str):
     guess_word = guess_word.strip().lower()
 
-    if len(guess_word) != 5:
-        message = "Please enter a valid 5 letter world"
+    if not validate_guess(guess_word):
+        message = "Please enter a valid 5 letter word"
         result = [""] * 5
 
     else:
         result = guess_attempt(guess_input=guess_word, answer=answer)
-        result_state = result.count("success")
-        if result_state == 5:
-            message = "Congrats. You have solved the wordle puzzle"
-
-        elif result_state in (3, 4):
-            message = "oof. Close. You're almost there"
-
-        elif result_state in (1, 2):
-            message = "Come on dawg. Lock in!"
-
-        else:
-            message = "Yea pack it up lil bro!"
+        message = response_builder(result)
 
     return {"message": message, "result": result}
