b4d7759e0b6bc09062c95fd75c76f653cfbbd412
Rahul Rajendran <rahulrajendran10098@gmail.com>
2025-12-27 21:30:44 +0530

    refactor: rename module to engine

---
+++ wordle/engine.py
@@ -0,0 +1,51 @@
+from enum import Enum
+type WordAndPositionMap = tuple[dict[int, str], dict[int, str]]
+
+class Answer(Enum):
+    GREY_COMPLETE_MISS = "miss"
+    YELLOW_PARTIAL_GUESS = "partial"
+    GREEN_CORRECT_GUESS = "success"
+
+
+def get_word_map(word: str) -> WordAndPositionMap:
+
+    count_map = {}
+    position_map = {}
+    # word_map = {index: char for index, char in enumerate(word)}
+
+    for index, char in enumerate(word):
+        if char not in count_map.keys():
+            count_map[char] = 1
+        else:
+            count_map[char] += 1
+        position_map[index] = char
+
+    return count_map, position_map
+
+
+def guess_attempt(guess_input: str, answer: str) -> list[str]:
+    
+
+    result = [None, None, None, None, None]
+    guess_count_map, guess_position_map = get_word_map(guess_input.lower())
+    answer_count_map, answer_position_map = get_word_map(answer)
+    # print(answer_count_map)
+
+    # pass 1: greens check
+    for index, char in guess_position_map.items():
+        if answer_position_map[index] == char:
+            result[index] = Answer.GREEN_CORRECT_GUESS.value
+            answer_count_map[char] -= 1
+
+
+    #pass 2: check for yellows and greys
+    for index, char in guess_position_map.items():
+        if result[index] is not None:
+            continue
+        elif answer_count_map.get(char, 0) > 0:
+            result[index] = Answer.YELLOW_PARTIAL_GUESS.value
+            answer_count_map[char] -= 1
+        else:
+            result[index] = Answer.GREY_COMPLETE_MISS.value
+    return result
+    
