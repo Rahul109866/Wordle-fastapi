13ce0e9124755d0212de5a3aceb931490956bd6e
Rahul Rajendran <rahulrajendran10098@gmail.com>
2025-12-28 11:25:56 +0530

    feature: add html rendered pages for the home and guess page

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Form
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 from wordle.engine import guess_attempt
 from pydantic import BaseModel
 from wordle.get_answer import get_answer
 from wordle.rules import response_builder, validate_guess
 
-
+templates = Jinja2Templates("templates")
 app = FastAPI()
 
 
@@ -15,24 +17,46 @@ class GuessResponse(BaseModel):
 
 answer = get_answer()
 
-print(answer)
-
 
 @app.get("/")
-def main():
-    return {"message": "Welcome to Wordle API! Guess using /guess/<your guess word>"}
+def main(request: Request, response_class=HTMLResponse):
+    return templates.TemplateResponse(
+        name="index.html",
+        context={
+            "request": request
+        }
+
+    )
 
 
-@app.get("/guess/{guess_word}", response_model=GuessResponse)
-def user_guess(guess_word: str):
+@app.get("/guess", response_class=HTMLResponse)
+def guess_page(request: Request):
+    return templates.TemplateResponse(name="guess.html", context={"request": request})
+
+
+@app.post("/guess", response_class=HTMLResponse)
+def guess_word_attempt(request: Request, guess_word: str = Form(alias="guess")):
     guess_word = guess_word.strip().lower()
 
     if not validate_guess(guess_word):
-        message = "Please enter a valid 5 letter word"
-        result = [""] * 5
+        return templates.TemplateResponse(
+            name="guess.html",
+            context={
+                "request": request,
+                "message": "Invalid Guess. Please enter a valid 5 letter word",
+                "guess": guess_word,
+            },
+        )
 
-    else:
-        result = guess_attempt(guess_input=guess_word, answer=answer)
-        message = response_builder(result)
+    result = guess_attempt(guess_word, answer)
+    message = response_builder(result)
 
-    return {"message": message, "result": result}
+    return templates.TemplateResponse(
+        name="guess.html",
+        context={
+            "request": request,
+            "message": message,
+            "result": result,
+            "guess": guess_word,
+        },
+    )
